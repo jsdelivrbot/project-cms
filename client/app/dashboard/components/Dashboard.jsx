a4bf08a2b988fe1a7c31c8fcb0f21201ca4f3f3b
Jason Kraus <zbyte64@gmail.com>
2015-12-11 19:46:09 -0800

    navbar to render active link

--- client/app/dashboard/components/Dashboard.jsx
+++ client/app/dashboard/components/Dashboard.jsx
@@ -11,34 +11,38 @@ function MenuLink({to, children}) {
   return <button type="link"><Link to={to}>{children}</Link></button>
 }
 
-
-function Dashboard({apps, children}) {
+function NavBar({pathname, apps}) {
   var subApps = _.filter(apps, app => app.baseUrl !== '/');
+  var currentApp = _.find(apps, app => _.startsWith(app.baseUrl, pathname));
 
-  return <div>
-    <nav className="navbar navbar-default" key="navbar">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
-          <Link className="navbar-brand" to="/">CMS</Link>
-        </div>
-
-        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul className="nav navbar-nav navbar-left">
-            {_.map(subApps, app => <li key={app.baseUrl}><Link to={app.baseUrl}>{app.title}</Link></li>)}
-          </ul>
-
-          <ul className="nav navbar-nav navbar-right">
-            <li><PublishButton/></li>
-          </ul>
-        </div>
+  return <nav className="navbar navbar-default" key="navbar">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>
+        <Link className="navbar-brand" to="/">CMS</Link>
       </div>
-    </nav>
+
+      <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul className="nav navbar-nav navbar-left">
+          {_.map(subApps, app => <li key={app.baseUrl} className={app === currentApp ? "active" : ""}><Link to={app.baseUrl}>{app.title}</Link></li>)}
+        </ul>
+
+        <ul className="nav navbar-nav navbar-right">
+          <li><PublishButton/></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+}
+
+function Dashboard({location, apps, children}) {
+  return <div>
+    <NavBar key="nav" pathname={location.pathname} apps={apps}/>
 
     <Alerts key="alerts"/>
 
