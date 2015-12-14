3b07443c1353b07be6b7951ad5058698e1ab15e9
Jason Kraus <zbyte64@gmail.com>
2015-12-14 14:51:53 -0800

    update notes

--- client/app/dashboard/components/Welcome.jsx
+++ client/app/dashboard/components/Welcome.jsx
@@ -31,8 +31,19 @@ export default function Welcome() {
           <li>Everything is a pluggable application and is given a `baseUrl` for mounting in the dashboard app</li>
           <li>Publishing currently exports to a zipfile</li>
           <li>Persistence is done with localstorage which can be <a onClick={doPurge}>purged</a></li>
-          <li>[TODO] apps can be hot loaded from github</li>
           <li>[TODO] HTTP/2 will dramatically decrease load time</li>
+          <li>I would hot load github modules and push to S3 or IPFS but cross origin security prevents that</li>
+          <li>Local storage maxes out at 5MB, so no full media app is possible for this demo</li>
+        </ul>
+
+        <p>
+          App ideas:
+        </p>
+        <ul>
+          <li>Blogging: much like pages but generates index pages</li>
+          <li>Gallery: upload a list of images and get a gallery page</li>
+          <li>Media: Core app, manage photos, videos, and other assets</li>
+          <li>CTA: Create call to actions that can be embedded into any template (using a swig template tag)</li>
         </ul>
       </div>
     </div>
